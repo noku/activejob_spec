8f46792de1f2023d86b4d98cdb942995a2e87766
Peter Negrei <negrei.petru@gmail.com>
2015-08-20 16:41:55 +0300

    add basic rspec setup

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -1,2 +1,11 @@
+require 'rails/all'
 $LOAD_PATH.unshift File.expand_path('../../lib', __FILE__)
 require 'activejob_spec'
+
+module ActiveJob
+  class Base
+    def self.queue_adapter
+      Struct.new(:enqueued_jobs).new(enqueued_jobs: [])
+    end
+  end
+end
