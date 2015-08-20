ba5237ded99fbfb69d9f71e2854936de8a470aa0
Peter Negrei <negrei.petru@gmail.com>
2015-08-20 18:25:00 +0300

    add #have_queued matcher

--- lib/activejob_spec/matchers.rb
+++ lib/activejob_spec/matchers.rb
@@ -2,20 +2,79 @@ require 'rspec/core'
 require 'rspec/expectations'
 require 'rspec/mocks'
 
-module InQueueHelper
+module ArgsHelper
+  def match_job(job, klass)
+    job.fetch(:job) == klass
+  end
+
+  def match_args(expected_args, args)
+    arg_list_matcher = RSpec::Mocks::ArgumentListMatcher.new(expected_args)
+    arg_list_matcher.args_match?(args)
+  end
+end
+
+module QueueHelper
+  include ArgsHelper
   include ActiveJob::TestHelper
 
+  def find_matching_jobs(actual, expected_args)
+    enqueued_jobs.select do |entry|
+      args = entry.fetch(:args)
+      match_job(entry, actual) && match_args(expected_args, args)
+    end
+  end
+
   def queue_size_for(klass)
-    enqueued_jobs.select { |job| job.fetch(:job) == klass }.size
+    enqueued_jobs.select { |job| match_job(job, klass) }.size
   end
 
   def enqueued_jobs
     ::ActiveJob::Base.queue_adapter.enqueued_jobs
   end
+
+  def check_size_for(matched_jobs, times)
+    if times
+      matched_jobs.size == times
+    else
+      matched_jobs.size > 0
+    end
+  end
+end
+
+RSpec::Matchers.define :have_queued do |*expected_args|
+  include QueueHelper
+
+  chain :times do |num_times_queued|
+    @times = num_times_queued
+    @times_info = (@times == 1 ? ' once' : " #{@times} times")
+  end
+
+  chain :once do
+    @times = 1
+    @times_info = ' once'
+  end
+
+  match do |actual|
+    matched_jobs = find_matching_jobs(actual, expected_args)
+
+    check_size_for(matched_jobs, @times)
+  end
+
+  failure_message do |actual|
+    "expected that #{actual} would have [#{expected_args.join(', ')}] queued#{@times_info}"
+  end
+
+  failure_message_when_negated do |actual|
+    "expected that #{actual} would not have [#{expected_args.join(', ')}] queued#{@times_info}"
+  end
+
+  description do
+    "have queued arguments of [#{expected_args.join(', ')}]#{@times_info}"
+  end
 end
 
 RSpec::Matchers.define :have_queue_size_of do |size|
-  include InQueueHelper
+  include QueueHelper
 
   match do |actual|
     queue_size_for(actual) == size
--- spec/activejob_spec/matchers_spec.rb
+++ spec/activejob_spec/matchers_spec.rb
@@ -50,4 +50,46 @@ describe 'ActiveJobSpec Matchers' do
       end
     end
   end
+
+  describe '#have_queued' do
+    context 'with a single job in the queue' do
+      before do
+        enqueued_jobs << { job: AJob, args: [1] }
+      end
+
+      it 'have the right parameter passed' do
+        expect(AJob).to have_queued(1)
+      end
+    end
+
+    context 'with multiple jobs in the queue' do
+      before do
+        enqueued_jobs << { job: AJob, args: [{ test: 1 }, 2] }
+        enqueued_jobs << { job: BJob, args: ['random', 3] }
+      end
+
+      it 'have right parameters for AJob' do
+        expect(AJob).to have_queued({ test: 1 }, 2)
+      end
+
+      it 'have right parameters for BJob' do
+        expect(BJob).to have_queued('random', 3)
+      end
+
+      it 'adds one job to the queue with right parameters' do
+        expect(BJob).to have_queued('random', 3).once
+      end
+    end
+
+    context 'with multiple jobs of the same class' do
+      before do
+        enqueued_jobs << { job: AJob, args: [1] }
+        enqueued_jobs << { job: AJob, args: [1] }
+      end
+
+      it 'has the right number of times for AJob' do
+        expect(AJob).to have_queued(1).times(2)
+      end
+    end
+  end
 end
